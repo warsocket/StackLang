b925a34102c6b67b68ce495f3cc128de142c12e8
Bram <warsocket@gmail.com>
2024-03-20 17:15:27 +0100

    Added bytecode compiling (to stdout) and bytetcode executing, added custom, non panic, error's

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,24 @@ use std::arch::asm;
 use std::io::{Read, Write, stdin, stdout, stderr};
 use std::collections::HashMap;
 
+const TOKENS:[u8;16] = [ //encoding 1 nibble pertoken, 2 per byte
+    b'(', // PUSH(reg)
+    b')', // POP to reg
+    b'+', // ADD
+    b'-', // SUB
+    b'*', // MUL
+    b'/', // DIV
+    b'|', // OR
+    b'&', // AND
+    b'^', // XOR
+    b'!', // NOT
+    b'1', // SHL 1; | 1
+    b'0', // SHL 1
+    b'$', // FUNCTION
+    b'@', // JUMP
+    b'=', // TEST (set flags for greater smaller etc), 0 result = same
+    b':', // SWITCH TO STACK #
+]; //Jumping outsid eof the array of instruction = HALT
 
 fn main() {
 
@@ -15,6 +33,14 @@ fn main() {
     let mut debug = false;
     let params:Vec<String> = args.collect();
     let mut filename = "".to_owned();
+
+    let mut mode = Mode::RUN;
+    enum Mode{
+        RUN,
+        COMPILE,
+        BYTECODE,
+    }
+
 
     // println!("{}", fil)
 
@@ -27,10 +53,10 @@ fn main() {
                     debug = true;
                 },
                 "--compile" => {
-                    todo!("comile to bytecode");
+                    mode = Mode::COMPILE;
                 },
                 "--bytecode" => {
-                    todo!("run bytecode");
+                    mode = Mode::BYTECODE;
                 },
                 _ => {
                     eprintln!("Unknown option '{}' !", param);
@@ -75,45 +101,100 @@ fn main() {
     script_bytes.resize(size, 0);
     file.read(&mut script_bytes);
 
-    let mut pure_script:Vec<u8> = vec!();
-    const valid_tokens:[u8;16] = [ //encoding 1 nibble pertoken, 2 per byte
-        b'(', // PUSH(reg)
-        b')', // POP to reg
-        b'+', // ADD
-        b'-', // SUB
-        b'*', // MUL
-        b'/', // DIV
-        b'|', // OR
-        b'&', // AND
-        b'^', // XOR
-        b'!', // NOT
-        b'1', // SHL 1; | 1
-        b'0', // SHL 1
-        b'$', // FUNCTION
-        b'@', // JUMP
-        b'=', // TEST (set flags for greater smaller etc), 0 result = same
-        b':', // SWITCH TO STACK #
-    ]; //Jumping outsid eof the array of instruction = HALT
+    match(mode){
+        Mode::RUN => { 
+            run(&tokenise(script_bytes), debug); 
+        },
+        Mode::COMPILE => { 
+            let mut out = stdout().lock();
+            out.write( &compile(&tokenise(script_bytes)) ); 
+        },
+        Mode::BYTECODE => { 
+            run(&bytecode(&script_bytes), debug); 
+        },
+    }
 
+}
+
+fn tokenise(script_bytes:Vec<u8>) -> Vec<u8>{
+
+    let mut pure_script:Vec<u8> = vec!();
     let mut ignore_to_newline = false;
+
     for token in script_bytes{
 
         if ignore_to_newline{
             if token == b'\n'{ ignore_to_newline = false; }
         }else{
-            if valid_tokens.contains(&token){
+            if TOKENS.contains(&token){
                 pure_script.push(token)
             }else if token == b'#'{
                 ignore_to_newline = true;
-            }            
+            }
         }
     }
 
-    run(&pure_script, debug)
+    pure_script
 
 }
 
-const STACK_ZERO:&str = "Error: Stack Empty.";
+fn compile(code:&Vec<u8>) -> Vec<u8>{
+    let mut opcodes:HashMap<u8, u8> = HashMap::new();
+
+    let mut high_bits = true;
+    let mut out:Vec<u8> = vec!();
+
+    for (index, token) in TOKENS.iter().enumerate(){
+        opcodes.insert(*token, index as u8);
+    }
+
+    //Since opcode 0 (PUSH(reg)) always works and is benign on itself, we dont care is lasat nibble contains just that.
+    for token in code{
+        if high_bits{
+            out.push( *opcodes.get(token).unwrap() << 4 );
+        }else{
+            let i:usize = out.len()-1;
+            out[i] |= *opcodes.get(token).unwrap();
+        }
+        high_bits = !high_bits;
+    }
+
+    out
+}
+
+
+fn bytecode(bytecode:&Vec<u8>) -> Vec<u8>{
+    let mut code:Vec<u8> = vec!();
+
+    for byte in bytecode{
+        code.push(TOKENS[usize::from(byte>>4)]);
+        code.push(TOKENS[usize::from(byte&0b1111)]);
+    }
+
+    code
+}
+
+const STACK_ZERO:&str = "Error: Stack Depleted!";
+
+
+trait Oos{
+    fn oos(self:&Self)->i64;
+}
+
+impl Oos for Option<i64>{
+    fn oos(self:&Self) -> i64{
+        match(self){
+            Some(v) => *v,
+            None => {
+                eprintln!("{}", STACK_ZERO);
+                exit(1);
+            }
+        }
+
+    }    
+}
+
+
 
 fn run(code:&Vec<u8>, debug:bool){
 
@@ -162,75 +243,75 @@ fn run(code:&Vec<u8>, debug:bool){
                 stack.push(reg);
             }
             b')' => {
-                reg = stack.pop().expect(STACK_ZERO);
+                reg = stack.pop().oos();
             }
             b'+' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 stack.push( (Wrapping(a)+Wrapping(b)).0 );
             }
             b'-' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 stack.push( (Wrapping(a)-Wrapping(b)).0 );
             }
             b'*' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 let x = i128::from(a)*i128::from(b);
                 stack.push((x >> 64) as i64);
                 stack.push(x as i64);
             }
             b'/' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 let x = i128::from(a)/i128::from(b);
                 stack.push((x >> 64) as i64);
                 stack.push(x as i64);
             }
             b'|' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 stack.push( a | b );
             }
             b'&' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 stack.push( a & b );
             }
             b'^' => {
-                let b = stack.pop().expect(STACK_ZERO);
-                let a = stack.pop().expect(STACK_ZERO);
+                let b = stack.pop().oos();
+                let a = stack.pop().oos();
                 stack.push( a ^ b );
             }
             b'!' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 stack.push( !a );
             }
             b'1' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 stack.push((a << 1) | 0b1);
             }
             b'0' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 stack.push((a << 1));
             }
             b'$' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 stack.push( func_matrix[reg as usize](a) );
             }
             b'@' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 index = a as usize;
                 step = false;
             }
             b'=' => {
                 // last 2 bits contain flags [negative, non-zero]
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 stack.push( ((a<0) as i64) <<1 | (a==0) as i64 )
             }
             b':' => {
-                let a = stack.pop().expect(STACK_ZERO);
+                let a = stack.pop().oos();
                 if stack.len() == 0 { stackmap.remove(&stackindex); } //prune empty maps you leave
 
                 if !stackmap.contains_key(&a){
